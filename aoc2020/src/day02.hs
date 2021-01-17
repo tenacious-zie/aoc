31fb09e0ff66a6b2c222dc50b7a657de884df9ad
Semyon <kontaktzielinski@protonmail.com>
2021-01-17 23:51:20 +0100

    im sad

--- aoc2020/src/day02.hs
+++ aoc2020/src/day02.hs
@@ -1,1 +1,23 @@
 module Day02 where
+
+import Prelude
+import Text.Parsec
+
+
+rights (Right x) = x
+rights (Left x) = "3"
+
+p :: Parsec String () (Int, Int, Char, String)
+p = do
+  low    <- many1 digit
+  string "-"
+  high   <- many1 digit
+  char ' '
+  lter <- letter
+  string ": "
+  wd <- many1 letter
+  return $ (read low, read high, lter, wd )
+
+main = do
+  xs <- lines <$> readFile "./data/day02.txt"
+  putStrLn $ rights $ parse p "" (head xs)
